fc0b998b592a8a85a49f554ef2e428af0b40b74b
JT Simon <jonsimn@amazon.com>
2022-04-08 11:31:36 -0500

    init

---
+++ cmd/add.go
@@ -0,0 +1,42 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/jon-simon/tri/todo"
+	"github.com/spf13/cobra"
+)
+
+// addCmd represents the add command
+var addCmd = &cobra.Command{
+	Use:   "add",
+	Short: "Add a new TODO",
+	Long:  `Add will create a new TODO item for you to get started on.`,
+	Run:   addRun,
+}
+
+func addRun(cmd *cobra.Command, args []string) {
+	items := []todo.Item{}
+	for _, x := range args {
+		items = append(items, todo.Item{Text: x})
+	}
+	fmt.Println(items)
+}
+
+func init() {
+	rootCmd.AddCommand(addCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// addCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// addCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
